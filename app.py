db0756712fc0f5125109d9784f12e54d3723e28c
richliu(刘笑江) <richliu@tencent.com>
2017-06-06 17:31:30 +0800

    web available

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask,request,render_template,redirect,url_for
+from flask.views import View
+from cli import *
+
+app = Flask(__name__)
+
+# http://127.0.0.1:5000/vid/2000007/like/101,202/hate/303,404
+@app.route('/')
+@app.route('/vid/<vid>')
+@app.route('/vid/<vid>/like/<likes_str>')
+@app.route('/vid/<vid>/hate/<hates_str>')
+@app.route('/vid/<vid>/like/<likes_str>/hate/<hates_str>')
+@app.route('/vid/<vid>/hate/<hates_str>/like/<likes_str>')
+def show_recommend(vid=None, likes_str=None, hates_str=None):
+    likes_param = request.args.get('like') if request.args.get('like') else None
+    hates_param = request.args.get('hate') if request.args.get('hate') else None
+    print('debug 0', request.args)
+    print('debug 1', hates_param)
+    likes = likes_str.split(',') if likes_str else [] + likes_param.split(',') if likes_param else []
+    hates = hates_str.split(',') if hates_str else [] + hates_param.split(',') if hates_param else []
+    print('debug 2', hates_str)
+    print('debug 3', hates)
+
+    if likes_param and not hates_param:
+        return redirect('/vid/%s/like/%s' % (vid, ','.join(likes)), code=302)
+    elif not likes_param and hates_param:
+        return redirect('/vid/%s/hate/%s' % (vid, ','.join(hates)), code=302)
+    elif likes_param and hates_param:
+        return redirect('/vid/%s/like/%s/hate/%s' % (vid, ','.join(likes), ','.join(hates)), code=302)
+
+
+    model = load_model()
+    
+    recommend_books = []
+    random_books = []
+    recent_books = get_recent_books(vid)[:10]
+
+    if vid and not likes:
+        likes = list(map(lambda x:x['bookId'], recent_books))
+    
+    random_books = get_random_books(model)
+    recommend_books = calc_recommend_books(model, likes, hates)
+
+    return render_template('index.html', vid=vid, 
+        recent_books=recent_books, 
+        recommend_books=recommend_books,
+        random_books=random_books,
+        like_books=list(map(lambda x:get_book_info(x), likes)),
+        hate_books=list(map(lambda x:get_book_info(x), hates)),
+        )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- cli.py
+++ cli.py
@@ -7,26 +7,53 @@ import random
 MODEL = 'output/word2vec.model'
 PREFS = ['input/shelfadd_201704.csv', 'input/user_prefs.txt'][0]
 SEP   = ' '
-MAX_CHOICE = 12
-MAX_RECOMM = 12
-CACHE = shelve.open('cache/shelve.cache')
+CACHE = {}#shelve.open('./shelve.cache')
 
+MAX_CHOICE_CLI = 12
+MAX_RECOMM_CLI = 12
+MAX_RECOMM_WEB = 30
+MAX_CHOICE_WEB = 30
 
+def jsonObj(url, use_cache=True):
+    if use_cache and url in CACHE.keys():
+        jObj = CACHE[url] 
+        return jObj
+    time.sleep(0.01 * random.random())
+    response = urllib.request.urlopen(url)
+    data = response.read()      # a `bytes` object
+    text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
+    jObj = json.loads(text)
+    CACHE[url] = jObj
+    return jObj
+
+# [(bookId, readUpdateTime), ...]
+def get_recent_books(vid):
+    ret = []
+    if not vid: return ret
+    vid = str(vid)
+    url = 'http://wr.qq.com:8080/shelf/sync?synckey=0&vid=%s' % vid
+    jObj = jsonObj(url)
+    if 'books' in jObj:
+        for bookinfo in jObj['books']:
+            if 'bookId' in bookinfo \
+                and 'readUpdateTime' in bookinfo \
+                and 'cover' in bookinfo \
+                and 'http://res.weread.qq.com/publicfetch?' not in bookinfo['cover']:
+                ret.append(bookinfo)
+    ret.sort(key=lambda x:x['readUpdateTime'], reverse=True)
+    return ret
+
+def get_book_info(bookId):
+    bookId = str(bookId)
+    if not bookId.isdigit(): return None
+    url = 'http://wr.qq.com:8080/book/info?bookId=%s' % bookId
+    return jsonObj(url, use_cache=True)
 
 def get_book_title(bookId):
-    def jsonObj(url, use_cache=True):
-        if use_cache and url in CACHE.keys():
-            jObj = CACHE[url] 
-            return jObj
-        time.sleep(0.05 * random.random())
-        text = urllib.request.urlopen(url).read().decode('utf-8') # a `str`; this step can't be used if data is binary
-        jObj = json.loads(text)
-        CACHE[url] = jObj
-        CACHE.sync()
-        return jObj
+    bookId = str(bookId)
     if not bookId.isdigit(): return bookId
-    url = 'http://wr.qq.com:8080/book/info?bookId=%s' % str(bookId)
-    jObj = jsonObj(url)
+    url = 'http://wr.qq.com:8080/book/info?bookId=%s' % bookId
+    jObj = jsonObj(url, use_cache=True)
     return (jObj['title'] if 'title' in jObj else '') + ' '+ (jObj['author'] if 'author' in jObj else '') 
 
 # {user:{dt1:item1, dt2:item2, ...}}
@@ -89,7 +116,7 @@ def print_vocabs(model, shuffle=False):
     vocabs = list(model.vocab.keys())
     print('choose:')
     print('\tindex\titem')
-    li = random.sample(list(range(len(vocabs))), MAX_CHOICE) if shuffle else list(range(MAX_CHOICE))
+    li = random.sample(list(range(len(vocabs))), MAX_CHOICE_CLI) if shuffle else list(range(MAX_CHOICE_CLI))
     for i in li:
         print('\t%d\t%s' % (i, get_book_title(vocabs[i])))
 
@@ -102,14 +129,38 @@ def print_recommend(model, pos, neg):
     print('\tindex\tscore\titem')
     most_similar = model.most_similar(positive=list(pos), negative=list(neg))
     for i in range(len(most_similar)):
-        if i > MAX_RECOMM:
+        if i > MAX_RECOMM_CLI:
             break
         vocab, score = most_similar[i]
         index = vocab_index(vocab, model)
         print('\t%d\t%.2f\t%s' % (index, score, get_book_title(vocab)))
     print('')
 
-def choose(model):
+def calc_recommend_books(model, pos, neg):
+    ret = []
+    vocabs = list(model.vocab.keys())
+    pos = set(filter(lambda x:x in vocabs, pos))
+    neg = set(filter(lambda x:x in vocabs, neg))
+    most_similar = model.most_similar(positive=list(pos), negative=list(neg))
+    for i in range(len(most_similar)):
+        if i > MAX_RECOMM_WEB:
+            break
+        vocab, score = most_similar[i]
+        index = vocab_index(vocab, model)
+        bookinfo = get_book_info(vocab)
+        if bookinfo:
+            bookinfo['score'] = ret
+            ret.append(bookinfo)
+    return ret
+
+def get_random_books(model):
+    ret = []
+    vocabs = list(model.vocab.keys())
+    for bookid in random.sample(vocabs, MAX_CHOICE_WEB):
+        ret.append(get_book_info(bookid))
+    return ret
+    
+def cli_choose(model):
     vocabs = list(model.vocab.keys())
     pos = set()
     neg = set()
@@ -143,13 +194,14 @@ def choose(model):
 
 
 def main():
+    # print(get_recent_books(2000007))
     random.seed(99)
-    prefs = read_prefs()
+    # prefs = read_prefs()
     # model = train_model(prefs)
     # save_model(model)
     model = load_model()
     # calc_item_cf(model)
-    choose(model)
+    cli_choose(model)
     
 if __name__ == '__main__':
     main()
