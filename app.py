039624bdad1ed9f3a611f987c7d4a8f421b87550
richliu(刘笑江) <richliu@tencent.com>
2017-06-07 15:51:18 +0800

    remove X button

--- app.py
+++ app.py
@@ -25,12 +25,15 @@ def calc_redirect_url_likes_hates_removes(request, likes_str, hates_str):
     likes += likes_str.split(',') if likes_str else []
     hates += hates_str.split(',') if hates_str else []
 
+    likes = list(set(likes) - set(hates) - set(removes))
+    hates = list(set(hates) - set(likes) - set(removes))
+
     url = ''
     if likes:
         url += '/like/' + ','.join(likes)
     if hates:
         url += '/hate/' + ','.join(hates)
-    if likes_param or hates_param:
+    if likes_param or hates_param or removes_param:
         return url, likes, hates, removes
     else:
         return None, likes, hates, removes
@@ -47,17 +50,12 @@ def index():
 @app.route('/vid/<vid>/hate/<hates_str>/like/<likes_str>')
 def show_recommend(vid=None, likes_str=None, hates_str=None):
     url, likes, hates, removes = calc_redirect_url_likes_hates_removes(request, likes_str, hates_str)
-    likes = list(set(likes) - set(hates) - set(removes))
-    hates = list(set(hates) - set(likes) - set(removes))
-
     if url:
         return redirect('vid/%s' % vid + url)
 
     recent_books = []
     if vid and vid != '0':
         recent_books = get_recent_books(vid)[:10]
-        if not likes:
-            likes = list(map(lambda x:x['bookId'], recent_books))[:3]
 
     random_books = get_random_books(model)
 
@@ -73,4 +71,5 @@ def show_recommend(vid=None, likes_str=None, hates_str=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+    # app.run()
